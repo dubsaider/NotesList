29be3c1b236da6eb9babf618554354d0a519c458
Kostenkov Vladislav <44964427+dubsaider@users.noreply.github.com>
2021-04-22 13:29:29 +1000

    Create About.vue

---
+++ About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">
+    <li>
+      <a @click="document.location='https://vk.com/dub41'"> vk </a>
+    </li>
+    <li>
+      <a @click="document.location='https://www.instagram.com/dubsaider'"> inst </a>
+    </li>
+    <li>
+      <a href='https://github.com/dubsaider'> git </a>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+name: "About"
+}
+</script>
+
+<style scoped>
+
+</style>
