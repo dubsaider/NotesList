6e735c0e0b37256d8553dfdbb67dbf0ff9bdcc28
Kostenkov Vladislav <44964427+dubsaider@users.noreply.github.com>
2021-04-22 13:29:44 +1000

    Add files via upload

---
+++ Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card">
+    Познаем front без регистрации и смс, не без помощи шарящих людей
+  </div>
+</template>
+
+<script>
+export default {
+name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
