b0e44966f2628556be0538f9002971c1364f79d6
Kostenkov Vladislav <44964427+dubsaider@users.noreply.github.com>
2021-04-22 13:29:38 +1000

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="navbar">
+    <h3>Notes App</h3>
+
+    <ul class="navbar-menu">
+      <li>
+        <router-link to="/home">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/note">Note</router-link>
+      </li>
+      <li>
+        <router-link to="/about">About</router-link>
+      </li>
+    </ul>
+  </header>
+  <div class="container with-nav">
+    <router-view />
+  </div>
+</template>
